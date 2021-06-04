b05ed89233bed8918441e3a79b37fdc015b68f1f
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-06-04 11:38:00 -0300

    adicionando api

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 // Importe o redux-thunk
 import rootReducer from '../reducers';
+const api = 'https://aws.random.cat/meow';
 
 // Aplique o middleware
 const store = createStore(
